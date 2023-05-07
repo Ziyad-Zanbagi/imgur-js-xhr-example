986ffb366856ca2d0f867541ee32d9f331f1f1cb
Khalid Alharbi <kalharbi@users.noreply.github.com>
2023-05-07 16:13:51 +0300

    make a single API request

--- script.js
+++ script.js
@@ -11,7 +11,13 @@ function requestAlbum() {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         if (req.readyState == 4 && req.status == 200) {
-            processAlbumRequest(req.responseText);
+            //processAlbumRequest(req.responseText);
+            let response = JSON.parse(req.responseText)
+            for(item of response.data){
+                let imgElem = document.createElement("img");
+                imgElem.src = item.link;
+                document.body.appendChild(imgElem);
+            }
         }
         else if (req.readyState == 4 && req.status != 200) {
             console.log(req.status + " Error with the imgur API: ", req.responseText);
@@ -20,36 +26,4 @@ function requestAlbum() {
     req.open('GET', 'https://api.imgur.com/3/album/' + albumId + '/images', true); // true for asynchronous     
     req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
     req.send();
-}
-
-function processAlbumRequest(response_text) {
-    var respObj = JSON.parse(response_text);
-    for (item of respObj.data.slice(0, 10)){
-        console.log(item)
-        requestImage(item.id);
-    }
-}
-
-function requestImage(imageHash) {
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            processImageRequest(req.responseText);
-        }
-        else if (req.readyState == 4 && req.status != 200) {
-            console.log("Error with the imgur API");
-        }
-    }
-    req.open("GET", "https://api.imgur.com/3/image/" + imageHash, true); // true for asynchronous     
-    req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
-    req.send();
-}
-
-function processImageRequest(response_text) {
-    var respObj = JSON.parse(response_text);
-    let imgElem = document.createElement("img");
-    imgElem.src = respObj.data.link;
-    ;
-    //imgElem.referrerpolicy="no-referrer";
-    document.body.appendChild(imgElem);
 }
