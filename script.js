569653314281ff5054b57442daa5d492902d4f95
Ziyad Al-Zanbagi <rayceon@gmail.com>
2023-05-18 19:50:22 +0300

    init

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const clientId = "ae46f54c86c85c3";
+const clientId = "36cabdb4bbe3c71";
+// 286e0785ddb0b82c020dc7ee4e087d6598fbd3ea
 
 var defaultAlbumId = '';
 
@@ -22,7 +23,48 @@ function requestAlbumXHR() {
             console.log(req.status + " Error with the imgur API: ", req.responseText);
         }
     }
-    req.open('GET', 'https://api.imgur.com/3/album/' + albumId + '/images', true); // true for asynchronous     
+    req.open('GET', 'https://api.imgur.com/3/album/' + albumId + '/images', true); // true for asynchronous   
+    // Authorization: Client-ID YOUR_CLIENT_ID  
     req.setRequestHeader('Authorization', 'Client-ID ' + clientId);
     req.send();
 }
+function requestAlbumFetch(){
+    let albumId = document.getElementById("albumIdField").value;
+    let resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "";
+    var requestOptions = {
+        method: 'GET'
+      };
+    fetch("https://api.imgur.com/3/album/" + albumId + "/images", requestOptions)
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        let response = JSON.parse(data)
+        for(item of response.data){
+            let imgElem = document.createElement("img");
+            imgElem.src = item.link;
+            resultDiv.appendChild(imgElem);
+        }
+    })
+    .catch((e) => {
+        console.error(e);
+    })
+}
+async function requestAlbumAsyncAwait() {
+    let albumId = document.getElementById("albumIdField").value;
+    let resultDiv = document.getElementById("result");
+    resultDiv.innerHTML = "";
+    var requestOptions = {
+        method: 'GET'
+      };
+    const response = await fetch("https://api.imgur.com/3/album/" + albumId + "/images", requestOptions); // Wait until the request completes.
+    const data = await response.json(); // waits until the response completes
+    console.log(data)
+    for(item of data.data){
+        let imgElem = document.createElement("img");
+        imgElem.src = item.link;
+        resultDiv.appendChild(imgElem);
+    }
+}
+
